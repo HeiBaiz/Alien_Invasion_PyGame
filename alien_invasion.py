8f33b48cb1e23b548674da46ea6811b4fc0a8f16
HeiBaiz <HeiBaiz@outlook.com>
2024-11-01 08:33:21 +0800

    代码开发至12.6.8--飞船--在全屏模式下运行游戏

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,6 +16,11 @@ class AlienInvasion:
         
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
+        
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        
         pygame.display.set_caption("Alien Invasion")
         
         self.ship = Ship(self)
@@ -23,19 +28,56 @@ class AlienInvasion:
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 侦听键盘和鼠标事件
-            for event in pygame.event.get():
-                # quit the game event
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            # 每次循环都填充背景色
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            
-            # update the display
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
+            
+            
+##################################################################
+
+
+    def _check_events(self):
+        """相应按键和鼠标事件"""
+        # 侦听键盘和鼠标事件
+        for event in pygame.event.get():
+            # 退出游戏
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+               self._check_keydown_events(event)
+                    
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+    
+    
+    def _check_keydown_events(self, event):
+        """响应按下"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_z:
+            self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+    
+    def _check_keyup_events(self, event):
+        """响应释放"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+    
+    def _update_screen(self):
+        # 每次循环都重绘屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
 
 ##################################################################
                 
--- settings.py
+++ settings.py
@@ -1,10 +1,12 @@
 class Settings:
     """存储游戏《外星人入侵》中所有设置的类"""
     
-    def __init__(self):    
+    def __init__(self):
         """初始化游戏设置"""
         # 屏幕素质
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.screen_width = 1640
+        self.screen_height = 960
+        self.bg_color = (57, 197, 187)
         
+        #飞船的设置
+        self.ship_speed = 10
