c29df3754317edfab15cd8c589142d02edd6589c
baini <1485890854@qq.com>
2024-11-01 09:12:16 +0800

    添加子弹射击，新增bullet.py文件

--- alien_Invasion.py
+++ alien_Invasion.py
@@ -3,6 +3,7 @@ import sys
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 from z02 import Z02
 
 class AlienInvasion:
@@ -15,14 +16,15 @@ class AlienInvasion:
         # 创建配置类
         self.settings = Settings()
         
-        #self.screen = pygame.display.set_mode(
-            #(self.settings.screen_width, self.settings.screen_height))
-        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        #self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        #self.settings.screen_width = self.screen.get_rect().width
+        #self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         self.z02 = Z02(self)
         
     def run_game(self):
@@ -30,6 +32,9 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
+            self.z02.update()
+            
             self._update_screen()
             self.clock.tick(60)
             
@@ -55,8 +60,23 @@ class AlienInvasion:
             # 向左移动飞船
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
-            #pygame.quit()
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            # 发射子弹
+            self._fire_bullet()
+            
+        elif event.key == pygame.K_d:
+            # 向右移动02
+            self.z02.moving_right = True
+        elif event.key == pygame.K_a:
+            # 向左移动02
+            self.z02.moving_left = True
+        elif event.key == pygame.K_w:
+            # 向上移动02
+            self.z02.moving_up = True
+        elif event.key == pygame.K_s:
+            # 向下移动02
+            self.z02.moving_down = True
         
             
     def _check_keyup_events(self, event):
@@ -67,12 +87,43 @@ class AlienInvasion:
         elif event.key == pygame.K_RIGHT:
             #停止移动
             self.ship.moving_right = False
+        elif event.key == pygame.K_d:
+            #停止移动
+            self.z02.moving_right = False
+        elif event.key == pygame.K_a:
+            #停止移动
+            self.z02.moving_left = False
+        elif event.key == pygame.K_w:
+            #停止移动
+            self.z02.moving_up = False
+        elif event.key == pygame.K_s:
+            #停止移动
+            self.z02.moving_down = False
+            
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入编组 bullets """
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+            
+    def _update_bullets(self):
+        """更新子弹的位置并删除已经消失的子弹"""
+        # 更新子弹位置
+        self.bullets.update()
+        
+        # 删除已消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
                 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
         
         # 每次循环都重绘屏幕
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
         self.z02.blitme()
         
--- z02.py
+++ z02.py
@@ -6,6 +6,7 @@ class Z02:
     def __init__(self,ai_game):
         """初始化02并设置其初始位置"""
         self.screen = ai_game.screen 
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         
         # 加载02图像并获取其外接矩形
@@ -13,7 +14,33 @@ class Z02:
         self.rect = self.image.get_rect()
         
         # 每个02都放在屏幕的中央
-        self.rect.midbottom = self.screen_rect.center
+        self.rect.midbottom = self.screen_rect.midbottom
+        
+        # 在02的属性 X 中存储一个浮点数
+        self.x = float(self.rect.x)
+        # 在02的属性 Y 中存储一个浮点数
+        self.y = float(self.rect.y)
+        
+        # 移动标志（02一开始不移动）
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+        
+    def update(self):
+        """根据移动标志调整02的位置"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.z02_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.z02_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.z02_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.z02_speed
+            
+        # 根据 self.x 更新 rect 对象
+        self.rect.x = self.x
+        self.rect.y = self.y
         
     def blitme(self):
         """在指定位置绘制02"""
